9180175b15b8def3e257419cd1337dc022db71b9
Fred <michael_fredericks@hotmail.com>
2016-03-08 19:19:52 -0500

    Converted from bitboard representation to mailbox, and finished load from FEN.

--- chessboard.py
+++ chessboard.py
@@ -1,12 +1,10 @@
-from bitarray import bitarray
-
 # Helper functions
 
 
-def algebraic_to_bitpos(algebraicpos):
+def algebraic_to_arraypos(algebraicpos):
     """
     :param algebraicpos: from "a1".."h8"
-    :return: an integer from 0..63 = space in the array corresponding to the square represented by algebraicpos
+    :return: an integer from 0..119 = space in the array corresponding to the square represented by algebraicpos
     """
 
     assert(len(algebraicpos) == 2)
@@ -18,28 +16,39 @@ def algebraic_to_bitpos(algebraicpos):
     assert('a' <= file <= 'h')
     assert(1 <= rank <= 8)
 
-    retval = 8 * (rank-1)
+    retval = (10 * (rank+1)) + 1
     retval += (ord(file)-97)  # 97 is the ascii for "a"
     return retval
 
 
-def bitpos_to_algebraic(bitpos):
+def arraypos_to_algebraic(bitpos):
     """
-    :param bitpos: an integer from 0..63 which is the space in the array corresponding to the square
+    :param bitpos: an integer from 0..119 which is the space in the array corresponding to the square
+                    however - only from 21..98 are actually on the board, excluding numbers ending in 0 or 9
     :return: an algebraic description of the square from "a1".."h8"
     """
-    assert (0 <= bitpos <= 63)
+    assert (21 <= bitpos <= 98)
+    assert (1 <= (bitpos % 10) <= 9)
 
-    file = chr(97 + (bitpos % 8))
-    rank = (bitpos // 8) + 1
+    file = chr(97 + (bitpos % 10) - 1)
+    rank = (bitpos // 10) - 1
     return file + str(rank)
 
 
+# layout of the board - count this way from 0..119
 
+# 110 111 112 113 114 115 116 117 118 119
+# 100 101 102 103 104 105 106 107 108 109
+# ...
+# 10 11 12 13 14 15 16 17 18 19
+# 00 01 02 03 04 05 06 07 08 09
 
+# all squares with 0 or 9 in the 1's space are off the board
+# all squares with tens digit 0 or 1 (which includes 100 and 110) are off the board
 class ChessBoard:
 
     def __init__(self):
+        self.board_array = list(120 * " ")
         self.white_castle_queen_side = True
         self.white_castle_king_side = True
         self.black_castle_queen_side = True
@@ -49,112 +58,139 @@ class ChessBoard:
         self.halfmove_clock = 0
         self.fullmove_number = 1
 
-        self.black_pawns_array = bitarray(64)
-        self.black_knights_array = bitarray(64)
-        self.black_bishops_array = bitarray(64)
-        self.black_rooks_array = bitarray(64)
-        self.black_queens_array = bitarray(64)
-        self.black_king_array = bitarray(64)
+        # To be concise, I would prefer to have init do nothing else, but need to define all members in __init__
+        self.erase_board()
 
-        self.white_pawns_array = bitarray(64)
-        self.white_knights_array = bitarray(64)
-        self.white_bishops_array = bitarray(64)
-        self.white_rooks_array = bitarray(64)
-        self.white_queens_array = bitarray(64)
-        self.white_king_array = bitarray(64)
+    def erase_board(self):
+        self.board_array = list(
+            'xxxxxxxxxx'
+            'xxxxxxxxxx'
+            'x        x'
+            'x        x'
+            'x        x'
+            'x        x'
+            'x        x'
+            'x        x'
+            'x        x'
+            'x        x'
+            'xxxxxxxxxx'
+            'xxxxxxxxxx')
 
-        self.black_pawns_array.setall(False)
-        self.black_knights_array.setall(False)
-        self.black_bishops_array.setall(False)
-        self.black_rooks_array.setall(False)
-        self.black_queens_array.setall(False)
-        self.black_king_array.setall(False)
-        self.white_pawns_array.setall(False)
-        self.white_knights_array.setall(False)
-        self.white_bishops_array.setall(False)
-        self.white_rooks_array.setall(False)
-        self.white_queens_array.setall(False)
-        self.white_king_array.setall(False)
+        self.white_castle_queen_side = True
+        self.white_castle_king_side = True
+        self.black_castle_queen_side = True
+        self.black_castle_king_side = True
+        self.white_to_move = True
+        self.en_passant_target_square = -1
+        self.halfmove_clock = 0
+        self.fullmove_number = 1
 
     def initialize_start_position(self):
-        self.black_pawns_array = bitarray("0000000000000000000000000000000000000000000000001111111100000000")
-        self.black_knights_array = bitarray("0000000000000000000000000000000000000000000000000000000001000010")
-        self.black_bishops_array = bitarray("0000000000000000000000000000000000000000000000000000000000100100")
-        self.black_rooks_array = bitarray("0000000000000000000000000000000000000000000000000000000010000001")
-        self.black_queens_array = bitarray("0000000000000000000000000000000000000000000000000000000000010000")
-        self.black_king_array = bitarray("0000000000000000000000000000000000000000000000000000000000001000")
+        self.erase_board()
+        # NOTE - you are looking at a mirror image up/down of the board below, the first line is the bottom of the board
+        self.board_array = list(
+            'xxxxxxxxxx'
+            'xxxxxxxxxx'
+            'xRNBQKBNRx'
+            'xPPPPPPPPx'
+            'x        x'
+            'x        x'
+            'x        x'
+            'x        x'
+            'xppppppppx'
+            'xrnbqkbnrx'
+            'xxxxxxxxxx'
+            'xxxxxxxxxx')
 
-        self.white_pawns_array = bitarray("0000000011111111000000000000000000000000000000000000000000000000")
-        self.white_rooks_array = bitarray("1000000100000000000000000000000000000000000000000000000000000000")
-        self.white_knights_array = bitarray("0100001000000000000000000000000000000000000000000000000000000000")
-        self.white_bishops_array = bitarray("0010010000000000000000000000000000000000000000000000000000000000")
-        self.white_queens_array = bitarray("0001000000000000000000000000000000000000000000000000000000000000")
-        self.white_king_array = bitarray("0000100000000000000000000000000000000000000000000000000000000000")
-
-        self.white_castle_queen_side = True
-        self.white_castle_king_side = True
-        self.black_castle_queen_side = True
-        self.black_castle_king_side = True
-        self.white_to_move = True
-        self.en_passant_target_square = -1
-        self.halfmove_clock = 0
-        self.fullmove_number = 1
-
-    def convert_bitboard_to_array(self):
-        board_array = [[" " for x in range(8)] for x in range(8)]
-        for i in range(64):
-            cur_square = " "
-            if self.black_pawns_array[i]:
-                cur_square = "p"
-            elif self.black_knights_array[i]:
-                cur_square = "n"
-            elif self.black_bishops_array[i]:
-                cur_square = "b"
-            elif self.black_rooks_array[i]:
-                cur_square = "r"
-            elif self.black_queens_array[i]:
-                cur_square = "q"
-            elif self.black_king_array[i]:
-                cur_square = "k"
-            elif self.white_pawns_array[i]:
-                cur_square = "P"
-            elif self.white_knights_array[i]:
-                cur_square = "N"
-            elif self.white_bishops_array[i]:
-                cur_square = "B"
-            elif self.white_rooks_array[i]:
-                cur_square = "R"
-            elif self.white_queens_array[i]:
-                cur_square = "Q"
-            elif self.white_king_array[i]:
-                cur_square = "K"
-
-            if cur_square != " ":
-                board_array[i // 8][i % 8] = cur_square
-
-        return board_array
+    def pretty_print(self):
+        outstr = ""
+        for i in range(90, 10, -10):
+            for j in range(1, 9, 1):
+                outstr += self.board_array[i+j]
+            outstr += "\n"
+        return outstr
 
     def load_from_fen(self, fen):
-        pass
+
+        self.erase_board()
+
+        # The FEN loads from a8 through h8, then a7 through h7, etc.
+        cur_square = algebraic_to_arraypos("a8")
+
+        for counter in range(fen.find(" ")):
+            cur_char = fen[counter]
+            if cur_char == "/":
+                # move to the next rank
+                cur_square -= 10
+                # now move to the first file in this rank
+                cur_square = (10 * (cur_square // 10)) + 1  # if this operation happens often, refactor into function.
+            elif "1" <= cur_char <= "8":
+                cur_square += int(cur_char)
+            else:
+                self.board_array[cur_square] = cur_char
+                cur_square += 1
+
+        counter = fen.find(" ") + 1
+        if fen[counter] == "w":
+            self.white_to_move = True
+        else:
+            self.white_to_move = False
+
+        counter += 2
+        self.black_castle_king_side = False
+        self.black_castle_queen_side = False
+        self.white_castle_king_side = False
+        self.white_castle_queen_side = False
+        while fen[counter] != " ":
+            if fen[counter] == "K":
+                self.white_castle_king_side = True
+            elif fen[counter] == "Q":
+                self.white_castle_queen_side = True
+            elif fen[counter] == "k":
+                self.black_castle_king_side = True
+            elif fen[counter] == "q":
+                self.black_castle_queen_side = True
+            counter += 1
+
+        counter += 1
+        if fen[counter] == '-':
+            self.en_passant_target_square = -1
+            counter += 2
+        else:
+            self.en_passant_target_square = algebraic_to_arraypos(fen[counter:counter+2])
+            counter += 3
+
+        numstr = ""
+        while fen[counter] != " ":
+            numstr += fen[counter]
+            counter += 1
+        self.halfmove_clock = int(numstr)
+
+        counter += 1
+        numstr = ""
+        while counter < len(fen):
+            numstr += fen[counter]
+            counter += 1
+        self.fullmove_number = int(numstr)
 
     def convert_to_fen(self):
 
         retval = ""
-        board_array = self.convert_bitboard_to_array()
-        for file in range(7, -1, -1):
+        for rank in range(90, 10, -10):
             num_blanks = 0
-            for rank in range(0, 8, 1):
-                if board_array[file][rank] == " ":
+            for file in range(1, 9, 1):
+                if self.board_array[rank + file] == " ":
                     num_blanks += 1
                 else:
                     if num_blanks > 0:
                         retval += str(num_blanks)
                         num_blanks = 0
-                    retval += board_array[file][rank]
+                    retval += self.board_array[rank + file]
 
             if num_blanks > 0:
                 retval += str(num_blanks)
-            retval += "/"
+            if rank > 20:
+                retval += "/"
 
         retval += " "
 
@@ -183,11 +219,10 @@ class ChessBoard:
         if self.en_passant_target_square == -1:
             retval += "-"
         else:
-            retval += bitpos_to_algebraic(self.en_passant_target_square)
+            retval += arraypos_to_algebraic(self.en_passant_target_square)
 
         retval += " "
 
         retval += str(self.halfmove_clock) + " " + str(self.fullmove_number)
 
         return retval
-
--- chessboard_test.py
+++ chessboard_test.py
@@ -1,10 +1,41 @@
 import chessboard
 
-def test_algebraic_to_bitpos_converters():
+def test_algebraic_to_arraypos_converters():
+    num_failures = 0
+    num_successes = 0
+
     for file in range(97, 105, 1):
         for rank in range (1, 9, 1):
             algebraicpos = chr(file) + str(rank)
-            converted_algebraicpos = chessboard.bitpos_to_algebraic(chessboard.algebraic_to_bitpos(algebraicpos))
+            converted_algebraicpos = chessboard.arraypos_to_algebraic(chessboard.algebraic_to_arraypos(algebraicpos))
             if converted_algebraicpos != algebraicpos:
                 print("Failure: ", algebraicpos, " converted to ", converted_algebraicpos)
+                num_failures += 1
+            else:
+                num_successes += 1
+    print ("Complete. Success: ", num_successes, " Failure: ", num_failures)
 
+
+
+def test_fen_load():
+    b = chessboard.ChessBoard()
+    b.erase_board()
+    #b.load_from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    #b.load_from_fen("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")
+    b.load_from_fen("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2")
+    print(b.pretty_print())
+    print(b.white_castle_king_side)
+    print(b.white_castle_queen_side)
+    print(b.black_castle_king_side)
+    print(b.black_castle_queen_side)
+    print(b.halfmove_clock)
+    print(b.fullmove_number)
+    print(b.en_passant_target_square)
+
+
+def test_fen_save():
+    b = chessboard.ChessBoard()
+    b.initialize_start_position()
+    print(b.convert_to_fen())
+
+# test_fen_load()
