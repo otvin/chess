7510090e5e0d1616d8c5398e64272ca95661357b
Fred <michael_fredericks@hotmail.com>
2016-03-14 23:42:04 -0400

    Defined the piece-square tables which will be the basis of the evaluation function yet to be written.

--- chess.py
+++ chess.py
@@ -9,9 +9,223 @@ from random import randint
 # Handle repeating position stalemate
 # Design a static evaluation function
 # Basic multi-ply with static evaluation
-# https://chessprogramming.wikispaces.com/Engine+Testing ; https://chessprogramming.wikispaces.com/Perft+Results
 # Support for command-line arguments
 # Wire in xboard
+# alpha-beta pruning
+# opening library
+# research into how to program endgames
+# transposition tables
+
+
+# initialization of piece-square-tables
+# layout of the board - count this way from 0..119
+
+# 110 111 112 113 114 115 116 117 118 119
+# 100 101 102 103 104 105 106 107 108 109
+# ...
+# 10 11 12 13 14 15 16 17 18 19
+# 00 01 02 03 04 05 06 07 08 09
+
+# start position is a list based on this (looks mirrored, keep in mind)
+#   'xxxxxxxxxx'
+#   'xxxxxxxxxx'
+#   'xRNBQKBNRx'
+#   'xPPPPPPPPx'
+#   'x        x'
+#   'x        x'
+#   'x        x'
+#   'x        x'
+#   'xppppppppx'
+#   'xrnbqkbnrx'
+#   'xxxxxxxxxx'
+#   'xxxxxxxxxx'
+
+ob = -32767  # short for "off board"
+
+white_pawn_pst = [
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, 0, 0, 0, 0, 0, 0, 0, 0, ob,
+    ob, 5, 10, 10, -20, -20, 10, 10, 5, ob,
+    ob, 5, -5, -10, 0, 0, -10, -5, 5, ob,
+    ob, 0, 0, 0, 20, 20, 0, 0, 0, ob,
+    ob, 5, 5, 10, 25, 25, 10, 5, 5, ob,
+    ob, 10, 10, 20, 30, 30, 20, 10, 10, ob,
+    ob, 50, 50, 50, 50, 50, 50, 50, 50, ob,
+    ob, 0, 0, 0, 0, 0, 0, 0, 0, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob
+]
+
+white_knight_pst = [
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, -50, -40, -30, -30, -30, -30, -40, -50, ob,
+    ob, -40, -20, 0, 5, 5, 0, -20, -40, ob,
+    ob, -30, 5, 10, 15, 15, 10, 5, -30, ob,
+    ob, -30, 0, 15, 20, 20, 15, 0, -30, ob,
+    ob, -30, 5, 15, 20, 20, 15, 5, -30, ob,
+    ob, -30, 0, 10, 15, 15, 10, 0, -30, ob,
+    ob, -40, -20, 0, 0, 0, 0, -20, -40, ob,
+    ob, -50, -40, -30, -30, -30, -30, -40, -50, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob
+]
+
+white_bishop_pst = [
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, -20, -10, -10, -10, -10, -10, -10, -20, ob,
+    ob, -10, 5, 0, 0, 0, 0, 5, -10, ob,
+    ob, -10, 10, 10, 10, 10, 10, 10, -10, ob,
+    ob, -10, 0, 10, 10, 10, 10, 0, -10, ob,
+    ob, -10, 5, 5, 10, 10, 5, 5, -10, ob,
+    ob, -10, 0, 5, 10, 10, 5, 0, -10, ob,
+    ob, -10, 0, 0, 0, 0, 0, 0, -10, ob,
+    ob, -20, -10, -10, -10, -10, -10, -10, 20, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob
+]
+
+white_rook_pst = [
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, 0, 0, 0, 5, 5, 0, 0, 0, ob,
+    ob, -5, 0, 0, 0, 0, 0, 0, -5, ob,
+    ob, -5, 0, 0, 0, 0, 0, 0, -5, ob,
+    ob, -5, 0, 0, 0, 0, 0, 0, -5, ob,
+    ob, -5, 0, 0, 0, 0, 0, 0, -5, ob,
+    ob, -5, 0, 0, 0, 0, 0, 0, -5, ob,
+    ob, 5, 10, 10, 10, 10, 10, 10, 5, ob,
+    ob, 0, 0, 0, 0, 0, 0, 0, 0, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob
+]
+
+white_queen_pst = [
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, -20, -10, -10, -5, -5, -10, -10, -20, ob,
+    ob, -10, 0, 5, 0, 0, 0, 0, -10, ob,
+    ob, -10, 5, 5, 5, 5, 5, 0, -10, ob,
+    ob, 0, 0, 5, 5, 5, 5, 0, -5, ob,
+    ob, -5, 0, 5, 5, 5, 5, 0, -5, ob,
+    ob, -10, 0, 5, 5, 5, 5, 0, -10, ob,
+    ob, -10, 0, 0, 0, 0, 0, 0, -10, ob,
+    ob, -20, -10, -10, -5, -5, -10, -10, -20, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob
+]
+
+# note -the page has a mid and end game, this is mid game, which makes it weak in end game
+white_king_pst = [
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, 20, 30, 10, 0, 0, 10, 30, 20, ob,
+    ob, 20, 20, 0, 0, 0, 0, 20, 20, ob,
+    ob, -10, -20, -20, -20, -20, -20, -20, -10, ob,
+    ob, -20, -30, -30, -40, -40, -30, -30, -20, ob,
+    ob, -30, -40, -40, -50, -50, -40, -40, -30, ob,
+    ob, -30, -40, -40, -50, -50, -40, -40, -30, ob,
+    ob, -30, -40, -40, -50, -50, -40, -40, -30, ob,
+    ob, -30, -40, -40, -50, -50, -40, -40, -30, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob,
+    ob, ob, ob, ob, ob, ob, ob, ob, ob, ob
+]
+
+# will initialize these programmatically later
+black_pawn_pst = list(120 * " ")
+black_knight_pst = list(120 * " ")
+black_bishop_pst = list(120 * " ")
+black_rook_pst = list(120 * " ")
+black_queen_pst = list(120 * " ")
+black_king_pst = list(120 * " ")
+
+
+
+def debug_print_pst(pst, name):
+    outstr = name + "\n"
+    for rank in range(90, 10, -10):
+        for file in range (1, 9, 1):
+            outstr += str(pst[rank + file]) + ", "
+        outstr += "\n"
+    print(outstr)
+
+def initialize_psts(is_debug = False):
+    # why am I doing this?  I could have added the value of pieces in the definition
+    # and defined the black pst's above instead of doing programatically.  Reason is that
+    # this way if I want to change the model slightly, I have to make the change in one place
+    # and it will percolate elsewhere automatically, instead of changing potentially dozens
+    # of values in multiple lists.
+
+    # add the value of the pieces to the pst's
+    pawn_value = 100
+    knight_value = 320
+    bishop_value = 330
+    rook_value = 500
+    queen_value = 900
+    king_value = 20000
+
+    for rank in range(90, 10, -10):
+        for file in range(1, 9, 1):
+            white_pawn_pst[rank + file] += pawn_value
+            white_knight_pst[rank + file] += knight_value
+            white_bishop_pst[rank + file] += bishop_value
+            white_rook_pst[rank + file] += rook_value
+            white_queen_pst[rank + file] += queen_value
+            white_king_pst[rank + file] += king_value
+
+    # to make the black pst's
+    # rank 20 maps to rank 90
+    # rank 30 maps to rank 80
+    # rank 40 maps to rank 70
+    # rank 50 maps to rank 60
+    # etc.
+    # rank 0,10,100,110 are off board
+
+    # initialize off-board ranks
+    for file in range(0, 10, 1):
+        for rank in [0, 10, 100, 110]:
+            black_pawn_pst[rank + file] = ob
+            black_knight_pst[rank + file] = ob
+            black_bishop_pst[rank + file] = ob
+            black_rook_pst[rank + file] = ob
+            black_queen_pst[rank + file] = ob
+            black_king_pst[rank + file] = ob
+
+    for file in range(0, 10, 1):
+        for rankflip in [(20, 90), (30, 80), (40, 70), (50, 60)]:
+            black_pawn_pst[rankflip[0] + file] = white_pawn_pst[rankflip[1] + file]
+            black_pawn_pst[rankflip[1] + file] = white_pawn_pst[rankflip[0] + file]
+            black_knight_pst[rankflip[0] + file] = white_knight_pst[rankflip[1] + file]
+            black_knight_pst[rankflip[1] + file] = white_knight_pst[rankflip[0] + file]
+            black_bishop_pst[rankflip[0] + file] = white_bishop_pst[rankflip[1] + file]
+            black_bishop_pst[rankflip[1] + file] = white_bishop_pst[rankflip[0] + file]
+            black_rook_pst[rankflip[0] + file] = white_rook_pst[rankflip[1] + file]
+            black_rook_pst[rankflip[1] + file] = white_rook_pst[rankflip[0] + file]
+            black_queen_pst[rankflip[0] + file] = white_queen_pst[rankflip[1] + file]
+            black_queen_pst[rankflip[1] + file] = white_queen_pst[rankflip[0] + file]
+            black_king_pst[rankflip[0] + file] = white_king_pst[rankflip[1] + file]
+            black_king_pst[rankflip[1] + file] = white_king_pst[rankflip[0] + file]
+
+
+    if is_debug:
+        debug_print_pst(white_pawn_pst, "White Pawn")
+        debug_print_pst(black_pawn_pst, "Black Pawn")
+        debug_print_pst(white_knight_pst, "White Knight")
+        debug_print_pst(black_knight_pst, "Black Knight")
+        debug_print_pst(white_bishop_pst, "White Bishop")
+        debug_print_pst(black_bishop_pst, "Black Bishop")
+        debug_print_pst(white_rook_pst, "White Rook")
+        debug_print_pst(black_rook_pst, "Black Rook")
+        debug_print_pst(white_queen_pst, "White Queen")
+        debug_print_pst(black_queen_pst, "Black Queen")
+        debug_print_pst(white_king_pst, "White King")
+        debug_print_pst(black_king_pst, "Black King")
+
+def evaluate_board(board):
+    # Evaluation function stolen from https://chessprogramming.wikispaces.com/Simplified+evaluation+function
+    pass
 
 
 def process_human_move(board):
@@ -97,6 +311,7 @@ def process_computer_move(board):
 
 
 def play_game(is_verbose = False, debug_fen = ""):
+    initialize_psts()
     b = chessboard.ChessBoard()
     if debug_fen == "":
         b.initialize_start_position()
@@ -124,4 +339,6 @@ def play_game(is_verbose = False, debug_fen = ""):
             done = not process_human_move(b)
 
 # play_game(True, "k7/8/pP6/8/8/8/Q7/K7 w - a7 145 102")
-play_game(True)
+# play_game(True)
+
+initialize_psts(True)
